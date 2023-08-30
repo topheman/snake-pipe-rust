b3338779353f08c46f1facee3a3a4383dd6ac13b
Christophe Rosset <tophe@topheman.com>
2023-08-30 22:31:38 +0200

    feat(cli): begin cli implementation with clap
    - https://rust-cli.github.io/book/index.html
    - https://github.com/clap-rs/clap/blob/7126f78421de4041c0de2f0c4a2141fa50f8b54a/examples/tutorial_derive/03_04_subcommands_alt.rs

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,6 +1,37 @@
+use clap::{Parser, Subcommand};
+
 use common::stream::process_io;
 
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Gamestate {
+        /// in ms
+        #[arg(short, long, default_value_t = 300)]
+        throttle: u32
+    },
+    Render
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Gamestate {throttle} => {
+            println!("called gamestate with throttle: {throttle:?}");
+        },
+        Commands::Render => {
+            println!("called render");
+        },
+    }
     println!("Hello, world!");
     process_io();
 }
