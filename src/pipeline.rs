112df0b4e340c694b8793309afa1b59db9f5f406
Christophe Rosset <tophe@topheman.com>
2024-02-28 20:53:47 +0100

    feat(#32): pipeline --list

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -18,24 +18,58 @@ pub enum Pipeline {
     HttpWatch,
 }
 
-pub fn generate_command(pipeline: Pipeline) {
+fn format_prefix(prefix: String) -> String {
+    if prefix.is_empty() {
+        return "".to_string();
+    }
+    return format!("{:12}", prefix);
+}
+
+pub fn generate_command(pipeline: Option<Pipeline>, list: bool, prefix: String) {
+    let prefix = format_prefix(prefix);
     match pipeline {
-        Pipeline::Play => println!("snakepipe gamestate|snakepipe render"),
-        Pipeline::Record => {
-            println!("snakepipe gamestate|tee /tmp/snakepipe-output|snakepipe render")
+        Some(Pipeline::Play) => println!("{}snakepipe gamestate|snakepipe render", prefix),
+        Some(Pipeline::Record) => {
+            println!(
+                "{}snakepipe gamestate|tee /tmp/snakepipe-output|snakepipe render",
+                prefix
+            )
         }
-        Pipeline::Replay => {
-            println!("cat /tmp/snakepipe-output|snakepipe throttle|snakepipe render")
+        Some(Pipeline::Replay) => {
+            println!(
+                "{}cat /tmp/snakepipe-output|snakepipe throttle|snakepipe render",
+                prefix
+            )
         }
-        Pipeline::SockPlay => {
-            println!("snakepipe gamestate|tee /tmp/snakepipe.sock|snakepipe render")
+        Some(Pipeline::SockPlay) => {
+            println!(
+                "{}snakepipe gamestate|tee /tmp/snakepipe.sock|snakepipe render",
+                prefix
+            )
         }
-        Pipeline::SockWatch => println!(
-            "cat /dev/null > /tmp/snakepipe.sock && tail -f /tmp/snakepipe.sock|snakepipe render"
+        Some(Pipeline::SockWatch) => println!(
+            "{}cat /dev/null > /tmp/snakepipe.sock && tail -f /tmp/snakepipe.sock|snakepipe render",
+            prefix
         ),
-        Pipeline::HttpPlay => {
-            println!("snakepipe gamestate|snakepipe render-browser|snakepipe render")
+        Some(Pipeline::HttpPlay) => {
+            println!(
+                "{}snakepipe gamestate|snakepipe render-browser|snakepipe render",
+                prefix
+            )
         }
-        Pipeline::HttpWatch => println!("snakepipe stream-sse|snakepipe render"),
+        Some(Pipeline::HttpWatch) => println!("{}snakepipe stream-sse|snakepipe render", prefix),
+        None => {
+            if list {
+                println!("List of pipelines\n");
+                generate_command(Some(Pipeline::Play), false, "play".to_string());
+                generate_command(Some(Pipeline::Record), false, "record".to_string());
+                generate_command(Some(Pipeline::Replay), false, "replay".to_string());
+                generate_command(Some(Pipeline::SockPlay), false, "sock-play".to_string());
+                generate_command(Some(Pipeline::SockWatch), false, "sock-watch".to_string());
+                generate_command(Some(Pipeline::HttpPlay), false, "http-play".to_string());
+                generate_command(Some(Pipeline::HttpWatch), false, "http-watch".to_string());
+                println!("\nCall snakepipe pipeline <COMMAND>|sh");
+            }
+        }
     }
 }
