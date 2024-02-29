b10d907067baa1acca48db003e6a624326eb70bd
Christophe Rosset <tophe@topheman.com>
2024-02-29 19:58:56 +0100

    feat(#32): format output of pipilines with owo-colors

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,4 +1,5 @@
 use clap::Subcommand;
+use owo_colors::OwoColorize;
 
 #[derive(Subcommand, Copy, Clone)]
 pub enum Pipeline {
@@ -18,57 +19,60 @@ pub enum Pipeline {
     HttpWatch,
 }
 
-fn format_prefix(prefix: String) -> String {
+fn print_formatted_pipeline(pipeline: &str, prefix: &str) {
     if prefix.is_empty() {
-        return "".to_string();
+        println!("{}", pipeline);
+    } else {
+        println!("  {:12}{}", prefix.bold(), pipeline);
     }
-    return format!("{:12}", prefix);
 }
 
-pub fn generate_command(pipeline: Option<Pipeline>, list: bool, prefix: String) {
-    let prefix = format_prefix(prefix);
+pub fn generate_command(pipeline: Option<Pipeline>, list: bool, prefix: &str) {
+    // let prefix = format_prefix(prefix);
     match pipeline {
-        Some(Pipeline::Play) => println!("{}snakepipe gamestate|snakepipe render", prefix),
-        Some(Pipeline::Record) => {
-            println!(
-                "{}snakepipe gamestate|tee /tmp/snakepipe-output|snakepipe render",
-                prefix
-            )
+        Some(Pipeline::Play) => {
+            print_formatted_pipeline("snakepipe gamestate|snakepipe render", prefix);
         }
-        Some(Pipeline::Replay) => {
-            println!(
-                "{}cat /tmp/snakepipe-output|snakepipe throttle|snakepipe render",
-                prefix
-            )
-        }
-        Some(Pipeline::SockPlay) => {
-            println!(
-                "{}snakepipe gamestate|tee /tmp/snakepipe.sock|snakepipe render",
-                prefix
-            )
-        }
-        Some(Pipeline::SockWatch) => println!(
-            "{}cat /dev/null > /tmp/snakepipe.sock && tail -f /tmp/snakepipe.sock|snakepipe render",
-            prefix
+        Some(Pipeline::Record) => print_formatted_pipeline(
+            "snakepipe gamestate|tee /tmp/snakepipe-output|snakepipe render",
+            prefix,
         ),
-        Some(Pipeline::HttpPlay) => {
-            println!(
-                "{}snakepipe gamestate|snakepipe render-browser|snakepipe render",
-                prefix
-            )
+        Some(Pipeline::Replay) => print_formatted_pipeline(
+            "cat /tmp/snakepipe-output|snakepipe throttle|snakepipe render",
+            prefix,
+        ),
+        Some(Pipeline::SockPlay) => print_formatted_pipeline(
+            "snakepipe gamestate|tee /tmp/snakepipe.sock|snakepipe render",
+            prefix,
+        ),
+        Some(Pipeline::SockWatch) => print_formatted_pipeline(
+            "cat /dev/null > /tmp/snakepipe.sock && tail -f /tmp/snakepipe.sock|snakepipe render",
+            prefix,
+        ),
+        Some(Pipeline::HttpPlay) => print_formatted_pipeline(
+            "snakepipe gamestate|snakepipe render-browser|snakepipe render",
+            prefix,
+        ),
+        Some(Pipeline::HttpWatch) => {
+            print_formatted_pipeline("snakepipe stream-sse|snakepipe render", prefix)
         }
-        Some(Pipeline::HttpWatch) => println!("{}snakepipe stream-sse|snakepipe render", prefix),
         None => {
             if list {
-                println!("List of pipelines\n");
-                generate_command(Some(Pipeline::Play), false, "play".to_string());
-                generate_command(Some(Pipeline::Record), false, "record".to_string());
-                generate_command(Some(Pipeline::Replay), false, "replay".to_string());
-                generate_command(Some(Pipeline::SockPlay), false, "sock-play".to_string());
-                generate_command(Some(Pipeline::SockWatch), false, "sock-watch".to_string());
-                generate_command(Some(Pipeline::HttpPlay), false, "http-play".to_string());
-                generate_command(Some(Pipeline::HttpWatch), false, "http-watch".to_string());
-                println!("\nCall snakepipe pipeline <COMMAND>|sh");
+                // todo format with anstyle https://github.com/clap-rs/clap/blob/d0028d74b507c6ce0a05cafd1f4c34bf7ec85c63/clap_builder/src/builder/styling.rs#L57
+                // todo remove reference to |sh - can't do that, it happens in a subshell, use pbcopy ?
+                println!("{}", "List of pipelines:".bold().underline());
+                generate_command(Some(Pipeline::Play), false, "play");
+                generate_command(Some(Pipeline::Record), false, "record");
+                generate_command(Some(Pipeline::Replay), false, "replay");
+                generate_command(Some(Pipeline::SockPlay), false, "sock-play");
+                generate_command(Some(Pipeline::SockWatch), false, "sock-watch");
+                generate_command(Some(Pipeline::HttpPlay), false, "http-play");
+                generate_command(Some(Pipeline::HttpWatch), false, "http-watch");
+                println!(
+                    "\nTo copy a pipeline, run: {}",
+                    "snakepipe pipeline <COMMAND>|pbcopy".bold()
+                );
+                // println!("{}", "snakepipe pipeline <COMMAND>|pbcopy".bold());
             }
         }
     }
