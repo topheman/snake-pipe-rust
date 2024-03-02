c1c3310128a37fa0f6d0dcf3b3899a0a9ca7b148
Christophe Rosset <tophe@topheman.com>
2024-03-02 09:55:08 +0100

    feat(#38): support localIp for dev-server-sse + switch to type=module

--- node-helpers/dev-server-sse/index.ts
+++ node-helpers/dev-server-sse/index.ts
@@ -1,9 +1,13 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
+import url from 'url';
+import localIpUrl from 'local-ip-url';
+import { parseGameStateFromAsyncIterator, InitOptions } from 'snakepipe';
 
-import { parseGameStateFromAsyncIterator } from 'snakepipe';
+import { makeServer } from './server.js'
 
-import { makeServer } from './server'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url)); // __dirname for esm
 
 if (process.argv.length < 3) {
   console.log("You must pass the path of the file containing the recording of a game.");
@@ -46,6 +50,17 @@ async function* infiniteAsyncGeneratorFromArrayString(input: Array<string>, dela
   }
 }
 
+function makeInitOptionWithLocalIp(initOptions: InitOptions, port: number): InitOptions {
+  const localIp = localIpUrl();
+  return {
+    ...initOptions,
+    metadatas: {
+      ...(initOptions.metadatas || {}),
+      'render-browser-host': `http://${localIp}:${port}`
+    }
+  }
+}
+
 async function main(resolvedFilePathOfGameRecording: string, port = 8080) {
   const staticFolder = path.resolve(__dirname, '../..', 'static');
   const fileContent =
@@ -55,7 +70,8 @@ async function main(resolvedFilePathOfGameRecording: string, port = 8080) {
       .filter(Boolean);
   const asyncGenerator = infiniteAsyncGeneratorFromArrayString(fileContent)
   const { options, lines } = await parseGameStateFromAsyncIterator(asyncGenerator);
-  makeServer({ options, lines }, staticFolder).listen({ port }).then(() => {
-    console.log(`Listening on http://localhost=${port}`)
+  const initOptionsWithLocalIp = makeInitOptionWithLocalIp(options, port);
+  makeServer({ options: initOptionsWithLocalIp, lines }, staticFolder).listen({ port, host: "0.0.0.0" }).then(() => {
+    console.log(`Listening on ${initOptionsWithLocalIp.metadatas?.['render-browser-host']}`);
   });
 }
